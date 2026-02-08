78815aafa4a4646cd70858be3e43db6cd005b120
DileDeSilva <dilesha.mymail@gmail.com>
2026-02-08 13:54:08 +0530

    4th

--- poss.py
+++ poss.py
@@ -5,13 +5,21 @@ class pos:
         self.invoice=[]
         self.payment_history=[]
 
-    def product_mgt(id,name,unit_price,qty):
+    def product_mgt(self,id,name,unit_price,qty):
         self.product.append({"id":id,"name":name,"unit_price":unit_price,"quantity":qty})
         return (f"product id {id}, {name}")
     
-    def customer_mgt(cusid,gender,cusname,loyalty_pt,promotion,contactNo)
+    def customer_mgt(self,cusid,gender,cusname,loyalty_pt,promotion,contactNo):
         self.customer.append({"id": cusid, "gender": gender, "name": cusname, "loyalty_points": loyalty_pt, "promotions": promotion, "contact_no": contactNo})
         return (f"customer id {cusid}, {cusname}")
+    
+    def invoice_mgt(self,invoiceNo,cusid,datePurchased,amt,totalPrice):
+        self.invoice.append({"invoiceNo":invoiceNo,"customer_id":cusid,"date":datePurchased,"amount":amt,"total_price":totalPrice})
+        return (f"invoice {invoiceNo} created")
+    
+    def pmt_mgt(self,cusid,invoiceNo):
+        self.payment_history.append({"customer_id":cusid,"invoiceNo":invoiceNo})
+        return (f"payment recorded for {invoiceNo}")
     
 #product input
 id=input("Enter product id: ")
