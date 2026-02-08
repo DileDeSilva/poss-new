0f4b14e827046a243ec253999d5f8e02d80cb18c
DileDeSilva <dilesha.mymail@gmail.com>
2026-02-08 13:40:29 +0530

    3rd

--- poss.py
+++ poss.py
@@ -13,6 +13,7 @@ class pos:
         self.customer.append({"id": cusid, "gender": gender, "name": cusname, "loyalty_points": loyalty_pt, "promotions": promotion, "contact_no": contactNo})
         return (f"customer id {cusid}, {cusname}")
     
+#product input
 id=input("Enter product id: ")
 if id.isdigit():
     id=int(id)
@@ -23,3 +24,15 @@ if unit_price.replace(".","").isdigit():
 qty=input("Enter quantity: ")
 if qty.isdigit():
     qty=int(qty)
+
+#customer input
+cusid=input("Enter customer id: ")
+if cusid.isdigit():
+    cusid=int(cusid)
+gender = input("Enter your gender: ")
+cusname = input("Enter customer name: ")
+loyalty_pt=input("Enter loyalty points: ")
+if loyalty_pt.isdigit():
+    loyalty_pt=int(loyalty_pt)
+promotion=input("Enter promotion: ")
+contactNo=input("Enter contact no: ")
